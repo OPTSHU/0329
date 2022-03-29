f82017cc0e3bd286d717a5b8f16155d3cc69eab9
OPTSHU <102577571+OPTSHU@users.noreply.github.com>
2022-03-29 15:13:52 +0800

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1648537293,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/mallet-sheet0.png",
+		"images/mouse-sheet0.png",
+		"images/mouse-sheet1.png",
+		"images/hole-sheet0.png",
+		"media/explosioncrunch_001.ogg",
+		"media/explosioncrunch_001.m4a",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
